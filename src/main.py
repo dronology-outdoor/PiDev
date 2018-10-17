a06092e60a95b4f08eb87eafa1feb5baf35da1e3
r4space <jane@wyngaard.co.za>
2018-10-17 14:10:33 -0400

    lsdir

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+from os import listdir
 from simple_settings import settings
 
 print (settings.LOG_FILE)
 f=open(settings.LOG_FILE,'w')
 f.write("Hello DOgs")
 f.close()
+
+os.listdir('/data/')
+os.listdir('./')
