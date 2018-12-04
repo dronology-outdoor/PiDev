8ea1ede8d16522d67efff9ff5da0fbfe69689a7c
r4space <jane@wyngaard.co.za>
2018-12-04 15:05:10 -0500

    Fixed ENV

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import time 
-#import NetworkConfig
-import dbus
+import NetworkConfig
+#import dbus
 
-sys_bus = dbus.SystemBus()
-#NetworkConfig.configure_adhoc()
+#sys_bus = dbus.SystemBus()
+NetworkConfig.configure_adhoc()
 
 while (1):
 	print ("BOGS")
