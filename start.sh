d511bb461446ea6bbe2134788f9e1c649dc0d8cf
r4space <jane@wyngaard.co.za>
2019-01-13 15:43:29 -0500

    seems to be working

--- start.sh
+++ start.sh
@@ -4,14 +4,13 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 echo "Setting adhoc wifi"
 python src/main.py --settings=adhoc_network
 echo "Network configured"
-i="0"
-while [ $i -lt 10 ]
-do
-echo "Hello, I'm waiting"
-pwd
-ls
-sleep 5
-done
+#i="0"
+#while [ $i -lt 10 ]
+#do
+#echo "Hello, I'm waiting"
+#pwd
+#ls
+#sleep 5
+#done
 echo "Starting pixhawk transmission script"
-python application/PiDronology/main.py --setttings=application/PiDronology/dronology_Settings
-
+python application/PiDronology/main.py --settings=dronology_Settings
