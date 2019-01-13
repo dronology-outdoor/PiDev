c82f0a09130d9fd853170489063d11e501337ccd
r4space <jane@wyngaard.co.za>
2019-01-13 13:46:04 -0500

    make container stay up

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import pi3_networking
+import time
 from simple_settings import settings
 
 if __name__ == "__main__":
     # Start adhoc interface
     pi3_networking.hotspot_control('wlan0', "up", settings.IP, settings.GATEWAY, settings.CONNECTION_UUID,
                                    settings.NETWORK_NAME)
+    while(1):
+        print("Just keeping container up")
+        time.sleep(10)
